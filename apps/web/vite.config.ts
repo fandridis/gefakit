4eae73d5f3c8084e1c77f3291fbd466530389c8d
Georgios Fandridis <fandridis@gmail.com>
2025-05-08 22:10:24 +0300

    test 1

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: 'react', autoCodeSplitting: true }),
     react(),
-    tailwindcss(), 
+    tailwindcss(),
     cloudflare()
   ],
   resolve: {
