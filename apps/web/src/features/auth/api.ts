b4ee5519c4991ed153c93854a0ad75163f1b5d0e
Georgios Fandridis <fandridis@gmail.com>
2025-05-08 13:47:17 +0300

    test

--- apps/web/src/features/auth/api.ts
+++ apps/web/src/features/auth/api.ts
@@ -2,6 +2,8 @@ import axios from 'redaxios';
 import { SignUpEmailRequestBodyDTO, GetSessionResponseDTO } from "@gefakit/shared";
 import { handleSimpleError } from '@/utils/api-error';
 
+console.log('import.meta.env.VITE_API_URL', import.meta.env.VITE_API_URL)
+
 const API_BASE_URL = import.meta.env.VITE_API_URL + '/api/v1/auth';
 
 // Type guard to check if an object conforms to the AppErrorResponse interface
