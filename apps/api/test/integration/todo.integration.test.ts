84d3488ecc1e3fb9ec1ccfd941d9f48ee7415ace
Georgios Fandridis <fandridis@gmail.com>
2025-05-09 15:57:21 +0300

    test

--- apps/api/test/integration/todo.integration.test.ts
+++ apps/api/test/integration/todo.integration.test.ts
@@ -21,6 +21,9 @@ describe('Todo API Integration Tests', () => {
   let sessionCookie: string;
 
   beforeAll(async () => {
+    console.log('process.env.TEST_DATABASE_URL', process.env.TEST_DATABASE_URL);
+    console.log('process.env.NODE_ENV', process.env.NODE_ENV);
+    console.log('process.env.DATABASE_URL', process.env.DATABASE_URL);
     const dbUrl = process.env.TEST_DATABASE_URL;
     if (!dbUrl) {
       throw new Error("TEST_DATABASE_URL environment variable not set.");
